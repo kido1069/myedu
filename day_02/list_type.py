0d94c9eff9515d612e4b0b022b238c3632ad7dbc
王伟栋 <wedhi@vip.qq.com>
2019-04-19 11:59:26 +0800

    新增 list 查询

---
+++ day_02/list_type.py
@@ -0,0 +1,11 @@
+def list_select(a):
+    print(a[2])
+    print(a[2:5])
+    print(a[0:6])
+    print(a[-2])
+    print(a[0:])
+
+
+if __name__ == '__main__':
+    alist = ['2',5,'eqw','8','无敌','牛皮','M2']
+    list_select(alist)
