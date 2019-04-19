6744d83e2bd7f526a4913e216fd558e3705dcac4
王伟栋 <wedhi@vip.qq.com>
2019-04-19 10:33:37 +0800

    first commit

---
+++ day01/base_type.py
@@ -0,0 +1,69 @@
+# def into_demo():
+#     print('xxx')
+#     print('www')
+
+# if __name__ == '__main__':
+#     print('hello')
+#     into_demo()
+
+
+# def wwd1():
+#     print('test1')
+# def wwd2():
+#     print('test2')
+# def wwd3():
+#     print('test3')
+
+# def wwd():
+#     aint = 5
+#     print(aint)
+#     print(type(aint))
+#
+# def str_demo():
+#     astr = '5'
+#     print(astr)
+#     print(type(astr))
+#
+# def float_demo():
+#     afloat = 3.14
+#     print(afloat)
+#     print(type(afloat))
+#
+# def wudi_demo(a,b):
+#     print(a+b)
+#     wc = 3
+#     print(type(int(wc)))
+#     print(type(str(wc)))
+#
+# def join_demo():
+#     a = 123
+#     b = '你好'
+#     c = 3.14
+#     print(str(a)+b+str(c))
+#     print('%s%s%s'%(a,b,c))
+#     print('a是%s b是%s c是%s'%(a,b,c))
+#
+def jiafa_demo(a,b):
+
+    print(a+b)
+
+
+def jianfa_demo(a,b):
+    c = a - b
+    return  c
+
+if __name__ == '__main__':
+    # wwd()
+    # str_demo()
+    # float_demo()
+    # wudi_demo(12,13)
+    # didi = 123
+    # wudi_demo('你好',str(didi))
+    # join_demo()
+    c = jianfa_demo(6,2)
+    # d = jiafa_demo(6,2)
+    # print(c)
+    # # print(type(c))
+    # print(d)
+    # print(type(d))
+
