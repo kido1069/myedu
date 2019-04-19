b0a3817f517fc04c1c31fb5c775b9058e50bc885
王伟栋 <wedhi@vip.qq.com>
2019-04-19 17:58:40 +0800

    负数

--- day_02/dict.type.py
+++ day_02/dict.type.py
@@ -1,5 +1,9 @@
+import  json
+
+
 adict = {"username":"yansl","password":"123456"}
 bdict = {5:"yansl","password":[2,5]}
+cdict_str = '{"username":"yansl","password":"123456"}'
 
 def ads():
     print(adict['username'])
@@ -15,8 +19,25 @@ def dict_ni():
     adict.update(bdict)
     print(adict)
 
-
+def dict_add1():
+    adict['sex'] = '男'
+    print(adict)
+# dumps字典转字符串
+def dict2str():
+    print(type(adict))
+    adict_str = json.dumps(adict)
+    print(adict_str)
+    print(type(adict_str))
+# loads字符串转字典
+def str2dict():
+    print(type(cdict_str))
+    cdict = json.loads(cdict_str)
+    print(cdict)
+    print(type(cdict))
 if __name__ == '__main__':
     # ads()
     # dict_n()
-    dict_ni()
+    # dict_ni()
+    # dict_add1()
+    # dict2str()
+    # str2dict()
---
+++ day_02/other_type.py
@@ -0,0 +1,8 @@
+if __name__ == '__main__':
+    abool = true
+    bbool = false
+    print(type(abool))
+
+    acomlex = 1+2j
+    print(acomlex)
+    print(type(acomlex))
